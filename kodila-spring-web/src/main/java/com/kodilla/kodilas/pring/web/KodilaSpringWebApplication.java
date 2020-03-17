7954c374461576912e77cb7ac36503eaa55b7c35
zxkuba <jakub.krzeszowiak@gmail.com>
2020-03-17 18:20:55 +0100

    4.0

---
+++ kodila-spring-web/src/main/java/com/kodilla/kodilas/pring/web/KodilaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilas.pring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilaSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodilaSpringWebApplication.class, args);
+    }
+
+}
---
+++ kodila-spring-web/src/test/java/com/kodilla/kodilas/pring/web/KodilaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilas.pring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodilaSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
